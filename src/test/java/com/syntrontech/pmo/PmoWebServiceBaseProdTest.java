fb5c352c2dd9376f3ba0ff0dc5c45072f207833b
ed_he <ed_he@syntrontech.com>
2018-08-12 23:31:04 +0800

    PmoWebServiceBaseProdTest need fix

--- src/test/java/com/syntrontech/pmo/PmoWebServiceBaseProdTest.java
+++ src/test/java/com/syntrontech/pmo/PmoWebServiceBaseProdTest.java
@@ -2,6 +2,7 @@ package com.syntrontech.pmo;
 
 import java.util.Arrays;
 
+import com.syntrontech.pmo.pmo.PmoSetting;
 import com.syntrontech.pmo.pmo.PmoWebServiceBaseProd;
 import com.syntrontech.pmo.pmo.PmoWebServiceSettings;
 import com.syntrontech.pmo.pmo.model.ResultMessage;
@@ -16,84 +17,74 @@ import org.mockito.Mock;
 import static org.mockito.Mockito.*;
 
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @RunWith(MockitoJUnitRunner.class)
-public class PmoWebServiceBaseProdTest { 
-	@Mock
-	private PmoWebServiceSettings settings;
-	@InjectMocks
-	private PmoWebServiceBaseProd pmoWs;
+public class PmoWebServiceBaseProdTest {
 
-	@Before
-	public void setUp() throws Exception {
-		doReturn("http://service.telecare.com.tw/MessageHubWS_test01/").when(settings).getUrl();
-		doReturn("Cyshb").when(settings).getPmoAccount();
-		doReturn("Test01gb").when(settings).getPmoCredentials();
-	}
 
-	@Test
-	public void testRegisterUser() throws Exception {
-		UserData user = new UserData();
-		user.setIdno("B123456789");
-		user.setPassword("B19860103");
-		user.setIdentifier("123456789");
-		user.setEmail("jason_chen@syntrontech.com");
-		user.setSex("M");
-		user.setTel("123456789");
-		user.setAreaCode("10010");
-		user.setBirth("1986/01/03");
-		ResultMessage message = pmoWs.registerUserByVendor(user);
-		System.out.println("testRegisterUser MESSAGE:" + message);
-	}
-	
-	@Test
-	public void testUploadUserData() throws Exception {
-		UserData user = new UserData();
-		user.setIdno("B123456789");
-		user.setPassword("B19860103");
-		user.setIdentifier("123456789");
-		user.setEmail("jason_chen@syntrontech.com");
-		user.setSex("M");
-		user.setTel("123456789");
-		user.setAreaCode("10010");
-		user.setBirth("1986/01/03");
-		ResultMessage message = pmoWs.uploadUserData(user);
-		System.out.println("testUploadUserData MESSAGE:" + message);
-	}
+    @Mock
+    private PmoWebServiceSettings settings;
+    @InjectMocks
+    private PmoWebServiceBaseProd pmoWs;
 
-	@Test
-	public void testGetUsers() throws Exception {
-	}
 
-	@Test
-	public void testValidateUser() throws Exception {
-	}
+    @Mock
+    private PmoSetting pmoWsSettings;
 
-//	@Test
-	public void testUploadVitalRecordVitalRecord() throws Exception {
-		VitalRecord record = new VitalRecord();
-		record.setIdNo("B123456789");
+    @Before
+    public void setUp() throws Exception {
+        doReturn("http://service.telecare.com.tw/MHWS_Test/").when(pmoWsSettings).getUrl();
+        doReturn("ttshb").when(pmoWsSettings).getPmoAccount();
+        doReturn("yier67").when(pmoWsSettings).getPmoCredentials();
+
+    }
+
+//    @Test
+//    public void testRegisterUser() throws Exception {
+//        UserData user = new UserData();
+//        user.setIdno("B123456789");
+//        user.setPassword("B19860103");
+//        user.setIdentifier("123456789");
+//        user.setEmail("jason_chen@syntrontech.com");
+//        user.setSex("M");
+//        user.setTel("123456789");
+//        user.setAreaCode("10010");
+//        user.setBirth("1986/01/03");
+//        ResultMessage message = pmoWs.registerUserByVendor(user);
+//        System.out.println("testRegisterUser MESSAGE:" + message);
+//    }
+
+//    @Test
+//    public void testUploadUserData() throws Exception {
+//        UserData user = new UserData();
+//        user.setIdno("B123456789");
+//        user.setPassword("B19860103");
+//        user.setIdentifier("123456789");
+//        user.setEmail("jason_chen@syntrontech.com");
+//        user.setSex("M");
+//        user.setTel("123456789");
+//        user.setAreaCode("10010");
+//        user.setBirth("1986/01/03");
+//        ResultMessage message = pmoWs.uploadUserData(user);
+//        System.out.println("testUploadUserData MESSAGE:" + message);
+//    }
+
+
+//    @Test
+//    public void testuploadVitalRecord() throws Exception {
+//        VitalRecord record = new VitalRecord();
+//        record.setIdNo("B123456789");
 //		record.setIdentifier("4143357370");
-		record.setLocationId("660010000101");
-		record.setType(VitalRecord.Type.BP);
-		record.setmTime("2015-04-15 13:40:00");
-		record.setInputType(VitalRecord.InputType.Device);
-		record.setValues(Arrays.asList(110, 75, 65));
-		ResultMessage message = pmoWs.uploadVitalRecord(record);
-		System.out.println("testUploadVitalRecordVitalRecord:" + message);
-	}
+//        record.setLocationId("660010000101");
+//        record.setType(VitalRecord.Type.BP);
+//        record.setmTime("2015-04-15 13:40:00");
+//        record.setInputType(VitalRecord.InputType.Device);
+//        record.setValues(Arrays.asList(110, 75, 65));
+//        ResultMessage message = pmoWs.uploadVitalRecord(record);
+//        System.out.println("testuploadVitalRecord:" + message);
+//    }
 
-	@Test
-	public void testGetVitalRecord() throws Exception {
-	}
-
-	@Test
-	public void testChangeUserPassword() throws Exception {
-	}
-
-	@Test
-	public void testResetUserPassword() throws Exception {
-	}
 
 }
